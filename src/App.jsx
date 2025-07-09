1f14a4d1ffb93ea276e1b933feadfb8206605ae2
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-10 01:50:44 +0300

    Setup Theme and Toaster providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./app/router";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-    return <RouterProvider router={router}></RouterProvider>
+    return (
+        <>
+            <Toaster position="top-right" toastOptions={{ duration: 1500, style: { margin: "130px 90px 0 0", fontSize: "0.85rem", padding: "2px" } }} />
+            <RouterProvider router={router}></RouterProvider>
+       </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/styles.css";
 import { UserProvider } from "./features/users/userContext";
+import { ThemeProvider } from "./app/context/themeContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <UserProvider>
-        <App />
-    </UserProvider>
+    <ThemeProvider>
+        <UserProvider>
+            <App />
+        </UserProvider>
+    </ThemeProvider>
 );
