ab421e192321633cf514e331e9c5b1bf6fe4f89d
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-09 18:59:08 +0300

    Adjust app layout

--- src/app/layout/AppLayout.jsx
+++ src/app/layout/AppLayout.jsx
@@ -1,31 +1,29 @@
 
-import { FaLeaf } from 'react-icons/fa';
+import { FaLeaf, FaMoon } from 'react-icons/fa';
 import { Outlet,Link } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            <nav className="bg-gray-800 p-4">
+        <div className="flex flex-col bg-[#111] pt-4 overflow-hidden h-screen">
+            <nav className="w-[85%] mx-auto">
                 <div className="flex items-center justify-between">
                     <div className="flex items-center">
-                        <FaLeaf className="text-white mr-2" />
-                        <Link to="/" className="text-white font-bold text-lg">Af-Users</Link>
+                        <FaLeaf className="text-[#fd4236] mr-1" />
+                        <Link to="/" className="text-white font-bold text-lg">Af<span className="text-[#fd4236]">Users</span></Link>
                     </div>
+
                     <div className="flex items-center">
-                        <input type="text" placeholder="Search" className="bg-gray-700 text-white rounded-md px-2 py-1 mr-2" />
                         <button className="text-white mr-2">
-                            <FaLeaf />
-                        </button>
-                        <button className="text-white">
-                            <FaLeaf />
+                            <FaMoon />
                         </button>
                     </div>
                 </div>
             </nav>
-            <div className="flex-grow">
+        
+            <div className="flex-grow w-[85%] mx-auto">
                 <Outlet />
             </div>
-            <footer className="bg-gray-800 p-4 text-white text-center">
+            <footer className="w-[85%] mx-auto p-4 text-white text-center">
                 <p>Powered By React</p>
             </footer>
         </div>
