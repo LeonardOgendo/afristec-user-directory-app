a941eb241f1e38a11fc69c733e3a12f564de9f71
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-09 19:00:19 +0300

    Wrap App in UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/styles.css";
+import { UserProvider } from "./features/users/userContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <App />
+    <UserProvider>
+        <App />
+    </UserProvider>
 );
