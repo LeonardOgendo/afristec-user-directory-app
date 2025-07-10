23eba61cacd1365c4c0286ffedad7f2535a68e16
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-10 07:32:16 +0300

    Add styles to Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import { Toaster } from "react-hot-toast";
 const App = () => {
     return (
         <>
-            <Toaster position="top-right" toastOptions={{ duration: 1500, style: { margin: "130px 90px 0 0", fontSize: "0.85rem", padding: "2px" } }} />
             <RouterProvider router={router}></RouterProvider>
+            
+            <Toaster 
+                position="top-right" 
+                toastOptions={{ 
+                    duration: 1500, 
+                    style: { margin: "130px 90px 0 0", fontSize: "0.85rem", padding: "2px", backgroundColor: "#e65100", color: "#fff8e7" }, 
+                    success: {
+                        iconTheme: {
+                            primary: "#fff8e7",
+                            secondary: "#e65100",
+                        },
+                    }  }} 
+            />
+
+            
        </>
     )
 }
